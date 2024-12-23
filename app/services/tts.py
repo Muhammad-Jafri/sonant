772762a6a7a0514bab0dd02de766048e605d0f13
Muhammad-jafri <muhammadjafri456@gmail.com>
2024-12-24 02:35:50 +0500

    feat: complete tts service

--- app/services/tts.py
+++ app/services/tts.py
@@ -1,3 +1,22 @@
-class Animal:
-    def __init__(self, animal : str) -> None:
-        self.animal = animal
+import wave
+
+from piper.voice import PiperVoice
+
+
+class TTSService:
+    def __init__(self, model_path: str):
+        self.model_path = model_path
+        self.voice = PiperVoice(model_path)
+
+    def generate_audio(self, text: str):
+        temp_wav_file = wave.open("output.wav", "w")
+        audio = self.voice.synthesize(text, temp_wav_file)
+        temp_wav_file.close()
+        # TODO return the base64 audio here
+
+    @staticmethod
+    def preprocess_text(text: str):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        pass
